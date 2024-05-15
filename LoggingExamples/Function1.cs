1c324cfb44ca19105a0ba4c951b935c625b09dbd
afdaniels@msn.com <alexdaniels@microsoft.com>
2024-05-15 14:26:05 -0700

    change to host.json

--- LoggingExamples/Function1.cs
+++ LoggingExamples/Function1.cs
@@ -17,7 +17,8 @@ namespace LoggingExamples
         [Function("Function1")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation("C# HTTP trigger function processed a request.", new { FunctionName = "MyHttpTrigger" });
+
             return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
